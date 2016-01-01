5b32866c8e6dd94c4d8d8e3145adcc5d63b15460
LeeYiFang <s89066@gmail.com>
2016-01-01 16:10:29 +0800

    simple home page
    simple home page(without database)

---
+++ Carkinos/Carkinos/settings/local.py
@@ -0,0 +1,11 @@
+from .base import *
+
+SECRET_KEY = 'br7jtj4&jwn=09^w*882xfcqm94b_#5h5zmtwwm#wtay+yx_0z'
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+    }
+}
---
+++ Carkinos/probes/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProbesConfig(AppConfig):
+    name = 'probes'
