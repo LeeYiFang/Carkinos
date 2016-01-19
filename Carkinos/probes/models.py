4af955e532ecc13b8a568159fa86c4022a80d2c6
LeeYiFang <s89066@gmail.com>
2016-01-20 04:04:47 +0800

    Simple website without using binary file
    The function without using row data is almost complete
    Add something to the models
    Can search for more than one keywords
    Datas are show in a more clear table

--- Carkinos/probes/models.py
+++ Carkinos/probes/models.py
@@ -2,25 +2,37 @@ from django.db import models
 
 # Create your models here.
 #U133A ,U133B(in the future) ...platform        
-'''class Platform(models.Model):
+class Platform(models.Model):
 
-    level = models.CharField(max_length=20)  #RNA
-    
+    name=models.CharField(max_length=50)
+    description=models.CharField(max_length=50)
     
     def __str__(self):
-        return self.name'''
+        return '%s of %s' % (self.name, self.description)
         
+class Sample(models.Model):
+    
+    name=models.CharField(max_length=50)
+    filename=models.CharField(max_length=50)
+    cell_line_id=models.ForeignKey('CellLine', related_name='fcell_line_id')
+    #dataset_id
+    platform_id=models.ForeignKey('Platform', related_name='fplatform_id')
+    
+    def __str__(self):
+        return '%s of %s' % (self.name, self.filename, self.cell_line_id.name,self.platform_id.name)
+
+
         
-#cellline_short(only 30)
+#Sanger project's cellline_short(only 30)
 class CellLine(models.Model):
 
     name = models.CharField(max_length=20)
     primary_site = models.CharField(max_length=50)
     primary_hist = models.CharField(max_length=50)
-    #platform = models.ForeignKey('Platform', related_name='plat_name')
+    
     
     def __str__(self):
-        return self.name
+        return '%s of %s' % (self.name, self.primary_site, self.primary_hist)
 
 
 #Affy_U133A_probe_info_short(only 30)
@@ -30,9 +42,10 @@ class ProbeID(models.Model):
     Gene_symbol = models.CharField(max_length=20)
     Entrez_id = models.IntegerField()
     Gene_name = models.TextField(blank=True, default='')
+    platform = models.ForeignKey('Platform', related_name='fplatform',default='')
     
     def __str__(self):
-        return self.Probe_id
+        return self.Probe_id  #Error: not all arguments converted during string
 
 
 
--- Carkinos/probes/views.py
+++ Carkinos/probes/views.py
@@ -11,10 +11,32 @@ def home(request):
     return render_to_response('home.html',locals())
 
 def data(request):
+    
     if 'cellline' in request.GET and request.GET['cellline']!='':
-        cell = CellLine.objects.get(name=request.GET['cellline'])
+        cell = CellLine.objects.filter(name=request.GET['cellline'])
+    else:
+        return HttpResponse("<p>where is the cell line?</p>")
     if  'keyword' in request.GET and request.GET['keyword']!='':
-        gene = ProbeID.objects.get(Gene_symbol=request.GET['keyword'])
+        words=request.GET['keyword']
+        words=words.split()
+        #return HttpResponse(words)
+    else:
+        return HttpResponse("<p>where is your keyword?</p>")
+        
+    gene=[]   
+    if 'gtype' in request.GET and request.GET['gtype']=='probeid':
+        for w in words:
+            gene+=ProbeID.objects.filter(Probe_id=w)
+        return render_to_response('data.html',locals()) 
+            
+    elif 'gtype' in request.GET and request.GET['gtype']=='symbol':
+        for w in words:
+            gene+=ProbeID.objects.filter(Gene_symbol=w)
+        return render_to_response('data.html',locals())
+            
+    elif 'gtype' in request.GET and request.GET['gtype']=='entrez':
+        for w in words:
+            gene+=ProbeID.objects.filter(Entrez_id=w)
         return render_to_response('data.html',locals())
     else:
-        return HttpResponse("<p>datas</p>")
+        return HttpResponse("<p>keyword type not match with your keyword input</p>")
