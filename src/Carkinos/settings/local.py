a039a48f5e30de936a5631643d880a89c86b7af5
Liang Bo Wang <ccwang002@gmail.com>
2016-01-26 11:39:10 +0800

    Directory restructure

--- src/Carkinos/settings/local.py
+++ src/Carkinos/settings/local.py
@@ -6,6 +6,6 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(os.path.dirname(BASE_DIR), 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
