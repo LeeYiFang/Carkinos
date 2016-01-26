2f28ae1cf81cb826ac83bc74f693940bef482946
Liang Bo Wang <ccwang002@gmail.com>
2016-01-26 12:22:23 +0800

    Fix PEP8

--- src/probes/views.py
+++ src/probes/views.py
@@ -2,43 +2,49 @@ from django.shortcuts import render,render_to_response
 from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_GET
 from .models import CellLine, ProbeID
-# Create your views here.
 
 
 def home(request):
-    
-    #return render(request, 'home.html')
-    return render_to_response('home.html',locals())
+    # return render(request, 'home.html')
+    return render_to_response('home.html', locals())
+
+
 def cell_lines(request):
-    lines=CellLine.objects.all()
-    return render_to_response('cell_line.html',locals())
+    lines = CellLine.objects.all()
+    return render_to_response('cell_line.html', locals())
+
+
 def data(request):
-    
-    if 'cellline' in request.GET and request.GET['cellline']!='':
-        cell = CellLine.objects.filter(name=request.GET['cellline'])
+
+    if 'cellline' in request.GET and request.GET['cellline'] != '':
+        cell = CellLine.objects.filter(name__in=request.GET['cellline'].split())
     else:
         return HttpResponse("<p>where is the cell line?</p>")
-    if  'keyword' in request.GET and request.GET['keyword']!='':
-        words=request.GET['keyword']
-        words=words.split()
-        #return HttpResponse(words)
+    if 'keyword' in request.GET and request.GET['keyword'] != '':
+        words = request.GET['keyword']
+        words = words.split()
+        # return HttpResponse(words)
     else:
         return HttpResponse("<p>where is your keyword?</p>")
-        
-    gene=[]   
-    if 'gtype' in request.GET and request.GET['gtype']=='probeid':
+
+    gene = []
+    if 'gtype' in request.GET and request.GET['gtype'] == 'probeid':
+        gene = ProbeID.objects.filter(Probe_id__in=words)
+        return render_to_response('data.html', {
+            'gene': gene,
+            'cell': cell,
+        })
+
+    elif 'gtype' in request.GET and request.GET['gtype'] == 'symbol':
         for w in words:
-            gene+=ProbeID.objects.filter(Probe_id=w)
-        return render_to_response('data.html',locals()) 
-            
-    elif 'gtype' in request.GET and request.GET['gtype']=='symbol':
+            gene += ProbeID.objects.filter(Gene_symbol=w)
+        return render_to_response('data.html', locals())
+
+    elif 'gtype' in request.GET and request.GET['gtype'] == 'entrez':
         for w in words:
-            gene+=ProbeID.objects.filter(Gene_symbol=w)
-        return render_to_response('data.html',locals())
-            
-    elif 'gtype' in request.GET and request.GET['gtype']=='entrez':
-        for w in words:
-            gene+=ProbeID.objects.filter(Entrez_id=w)
-        return render_to_response('data.html',locals())
+            gene += ProbeID.objects.filter(Entrez_id=w)
+        return render_to_response('data.html', locals())
     else:
-        return HttpResponse("<p>keyword type not match with your keyword input</p>")
+        return HttpResponse(
+            "<p>keyword type not match with your keyword input</p>"
+        )
