fc57486e027d5e3e6e264c2e49f0aad070640c1e
LeeYiFang <s89066@gmail.com>
2016-02-17 20:16:24 +0800

    models and homepage
    trying to use jquery

--- src/probes/models.py
+++ src/probes/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 
+class Dataset(models.Model):
+    
+    name=models.CharField(max_length=50)
+    data_path=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return '%s of %s' % (self.name, self.data_path)
+        
+
+
 # Create your models here.
-#U133A ,U133B(in the future) ...platform        
+       
 class Platform(models.Model):
 
     name=models.CharField(max_length=50)
@@ -15,15 +25,15 @@ class Sample(models.Model):
     name=models.CharField(max_length=50)
     filename=models.CharField(max_length=50)
     cell_line_id=models.ForeignKey('CellLine', related_name='fcell_line_id')
-    #dataset_id
+    dataset_id=models.ForeignKey('Dataset',related_name='fdataset_id',default='')
     platform_id=models.ForeignKey('Platform', related_name='fplatform_id')
     
     def __str__(self):
-        return '%s of %s' % (self.name, self.filename, self.cell_line_id.name,self.platform_id.name)
+        return 'name=%s filename=%s' % (self.name, self.filename)
 
 
         
-#Sanger project's cellline_short(only 30)
+
 class CellLine(models.Model):
 
     name = models.CharField(max_length=20)
@@ -32,10 +42,10 @@ class CellLine(models.Model):
     
     
     def __str__(self):
-        return '%s of %s' % (self.name, self.primary_site, self.primary_hist)
+        return '%s of %s of %s' % (self.name, self.primary_site, self.primary_hist)
 
 
-#Affy_U133A_probe_info_short(only 30)
+#(still 30)
 class ProbeID(models.Model):
 
     Probe_id = models.CharField(max_length=20)
--- src/probes/views.py
+++ src/probes/views.py
@@ -2,52 +2,53 @@ from django.shortcuts import render,render_to_response
 from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_GET
 from .models import CellLine, ProbeID
+from django.template import RequestContext
 
 
 def home(request):
     # return render(request, 'home.html')
-    return render_to_response('home.html', locals())
+    return render_to_response('home.html', RequestContext(request, locals()))
 
 
 def cell_lines(request):
     lines = CellLine.objects.all()
-    return render_to_response('cell_line.html', locals())
+    return render_to_response('cell_line.html', RequestContext(request, locals()))
 
 
 def data(request):
 
-    if 'cellline' in request.GET and request.GET['cellline'] != '':
-        cell = CellLine.objects.filter(name__in=request.GET['cellline'].split())
+    if 'cellline' in request.POST and request.POST['cellline'] != '':
+        cell = CellLine.objects.filter(name__in=request.POST['cellline'].split())
     else:
         return HttpResponse("<p>where is the cell line?</p>")
-    if 'keyword' in request.GET and request.GET['keyword'] != '':
-        words = request.GET['keyword']
+    if 'keyword' in request.POST and request.POST['keyword'] != '':
+        words = request.POST['keyword']
         words = words.split()
         # return HttpResponse(words)
     else:
         return HttpResponse("<p>where is your keyword?</p>")
 
     gene = []
-    if 'gtype' in request.GET and request.GET['gtype'] == 'probeid':
+    if 'gtype' in request.POST and request.POST['gtype'] == 'probeid':
         gene = ProbeID.objects.filter(Probe_id__in=words)
-        return render_to_response('data.html', {
+        return render_to_response('data.html', RequestContext(request,{
             'gene': gene,
             'cell': cell,
-        })
+        }))
 
-    elif 'gtype' in request.GET and request.GET['gtype'] == 'symbol':
+    elif 'gtype' in request.POST and request.POST['gtype'] == 'symbol':
         gene = ProbeID.objects.filter(Gene_symbol__in=words)
-        return render_to_response('data.html', {
+        return render_to_response('data.html', RequestContext(request,{
             'gene': gene,
             'cell': cell,
-        })
+        }))
 
-    elif 'gtype' in request.GET and request.GET['gtype'] == 'entrez':
+    elif 'gtype' in request.POST and request.POST['gtype'] == 'entrez':
         gene = ProbeID.objects.filter(Entrez_id=words)
-        return render_to_response('data.html', {
+        return render_to_response('data.html', RequestContext(request,{
             'gene': gene,
             'cell': cell,
-        })
+        }))
     else:
         return HttpResponse(
             "<p>keyword type not match with your keyword input</p>"
