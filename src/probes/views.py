7406dc7b6750243d680afd4f392f0c246df95c68
LeeYiFang <s89066@gmail.com>
2016-02-24 01:09:32 +0800

    checkbox
    Add checkbox, but not hide until dataset select
    Big problem: load very slowly(how to fix?)

--- src/probes/views.py
+++ src/probes/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render,render_to_response
 from django.http import HttpResponse, Http404
 from django.views.decorators.http import require_GET
-from .models import CellLine, ProbeID
+from .models import CellLine, ProbeID, Sample
 from django.template import RequestContext
 
 
 def home(request):
     # return render(request, 'home.html')
-    return render_to_response('home.html', RequestContext(request, locals()))
+    
+    known_cell_lines = {}
+    set0 = Sample.objects.filter(dataset_id__in='1')
+    for s in set0:
+        cl=s.cell_line_id.primary_site
+        try:
+            test=known_cell_lines[cl]
+        except KeyError:
+            if cl!='nan':
+                known_cell_lines[cl]=cl
+    keys1=known_cell_lines.keys()
+    return render_to_response('home.html', RequestContext(request,locals()))
 
 
 def cell_lines(request):
@@ -17,6 +28,7 @@ def cell_lines(request):
 
 def data(request):
 
+    
     if 'cellline' in request.POST and request.POST['cellline'] != '':
         cell = CellLine.objects.filter(name__in=request.POST['cellline'].split())
     else:
