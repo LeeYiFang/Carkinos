06dee4a4860764aad4692a55ee4ebb1827e3d309
LeeYiFang <s89066@gmail.com>
2016-10-28 06:50:19 +0800

    [pca test]
    and other function

--- src/probes/models.py
+++ src/probes/models.py
@@ -9,6 +9,14 @@ class Dataset(models.Model):
     
     def __str__(self):
         return '%s of %s' % (self.name, self.data_path)
+
+class Clinical_Dataset(models.Model):
+    
+    name=models.CharField(max_length=50)
+    data_path=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return '%s of %s' % (self.name, self.data_path)
         
 
      
@@ -34,6 +42,27 @@ class Sample(models.Model):
     def __str__(self):
         return 'name=%s filename=%s' % (self.name, self.filename)
 
+class Clinical_sample(models.Model):
+    
+    name=models.CharField(max_length=20)
+    filename=models.CharField(max_length=50)
+    dataset_id=models.ForeignKey('Clinical_Dataset',related_name='clinic_dataset_id',default='')
+    platform_id=models.ForeignKey('Platform', related_name='clinc_platform_id')
+    offset=models.IntegerField(default=0)
+    age=models.CharField(max_length=10,default='UNKNOWN')
+    gender=models.CharField(max_length=10,default='UNKNOWN')
+    ethnic=models.CharField(max_length=20,default='UNKNOWN')
+    grade=models.CharField(max_length=10)
+    stage=models.CharField(max_length=10)
+    primary_site= models.CharField(max_length=50)
+    primary_hist= models.CharField(max_length=100)
+    stageT=models.CharField(max_length=10,default='UNKNOWN')
+    stageN=models.CharField(max_length=10,default='UNKNOWN')
+    stageM=models.CharField(max_length=10,default='UNKNOWN')
+    metastatic=models.BooleanField(default=False)
+    
+    def __str__(self):
+        return '%s of %s of %s of %s' % (self.name, self.primary_site, self.primary_hist,self.dataset_id)
 
         
 #need to update with new datasets
--- src/probes/urls.py
+++ src/probes/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import url
 from .views import cellline_microarray  # explicit relative import
 from .views import similar_assessment
-from .views import welcome,pca,help_similar_assessment,gene_signature,heatmap,express_profiling,user_pca
+from .views import welcome,pca,help_similar_assessment,gene_signature,heatmap,express_profiling,user_pca,sample_microarray,clinical_search
 urlpatterns = [  
     url(r'^cellline_microarray$', cellline_microarray, name="cellline_microarray"),
     url(r'^similar_assessment$', similar_assessment, name="similar_assessment"),
@@ -13,5 +13,7 @@ urlpatterns = [
     url(r'^help_similar_assessment$',help_similar_assessment,name="help_similar_assessment"),
     url(r'^express_profiling$',express_profiling,name="express_profiling"),
     url(r'^user_pca$',user_pca,name="user_pca"),
+    url(r'^clinical_search$',clinical_search,name="clinical_search"),
+    url(r'^sample_microarray$', sample_microarray, name="sample_microarray"),
     
 ]
